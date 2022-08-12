84fb9eff8545609336677569a3252d786950ab2a
Ray Yang <ruiguo@gmail.com>
2022-08-12 15:57:54 -0700

    Test the m function

--- tests/generated/wrapper/test_mock_api_wrapper_g.py
+++ tests/generated/wrapper/test_mock_api_wrapper_g.py
@@ -1,6 +1,22 @@
-from wrapper.mock_api_wrapper import get_normalized_mock_calls_wrapper
+import collections
+import test_data.simple
+import test_scribe.global_var
+from unittest.mock import patch
+from wrapper.mock_api_wrapper import get_normalized_mock_calls_wrapper, m_wrapper
 
 
 def test_get_normalized_mock_calls_wrapper():
     result = get_normalized_mock_calls_wrapper()
     assert result == [('search_a_name', (), {'keyword': 'a'}), ('search_person', (), {'name': 'b'})]
+
+
+def test_m_wrapper_default_no_name():
+    with patch('test_scribe.global_var.g_mock_name_counter', collections.Counter(test_scribe.global_var.g_mock_name_counter)), patch('test_scribe.global_var.g_name_mock_dict', {}):
+        result = m_wrapper(name='')
+    assert result == (test_data.simple.C, 'm_c')
+
+
+def test_m_wrapper():
+    with patch('test_scribe.global_var.g_mock_name_counter', collections.Counter(test_scribe.global_var.g_mock_name_counter)), patch('test_scribe.global_var.g_name_mock_dict', {}):
+        result = m_wrapper(name='a')
+    assert result == (test_data.simple.C, 'a')
--- tests/wrapper/mock_api_wrapper.py
+++ tests/wrapper/mock_api_wrapper.py
@@ -1,12 +1,27 @@
 from unittest.mock import Mock
 
+from fixture.helper import patch_globals_modified_by_mock_proxy
 from test_data.service import Service
-from test_scribe.api.mock_api import get_normalized_mock_calls
+from test_data.simple import C
+from test_scribe.api.mock_api import get_normalized_mock_calls, m
+from test_scribe.mock_proxy import MockProxy
 
 
 def get_normalized_mock_calls_wrapper():
-    m = Mock(Service)
-    child_mock = m.search_a_name("a")
+    m_service = Mock(Service)
+    child_mock = m_service.search_a_name("a")
     child_mock.foo(1)
-    m.search_person("b")
-    return get_normalized_mock_calls(mock=m, spec=Service)
+    m_service.search_person("b")
+    return get_normalized_mock_calls(mock=m_service, spec=Service)
+
+
+def m_wrapper(name: str):
+    """
+    Since the return value is a MockProxy which is treated differently by the tool,
+    this wrapper is needed to get around the issue.
+    :return:
+    """
+    patch_globals_modified_by_mock_proxy()
+    m_proxy = m(spec=C, name=name) if name else m(spec=C)
+    assert isinstance(m_proxy, MockProxy)
+    return m_proxy.spec_test_scribe_, m_proxy.name_test_scribe_
