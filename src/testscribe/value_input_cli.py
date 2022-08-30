f0fe1065831f2a6745cdb39f8544e0f08a7f3d5a
Ray Yang <ruiguo@gmail.com>
2022-08-30 15:05:34 -0700

    Add Apache v2 license
    Add license notice to the production source code.
    See https://choosealicense.com/licenses/apache-2.0/

--- src/testscribe/value_input_cli.py
+++ src/testscribe/value_input_cli.py
@@ -1,3 +1,18 @@
+#  Copyright 2022 Ruiguo (Ray) Yang
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+#
+
 from __future__ import annotations
 
 from distutils.util import strtobool
@@ -5,6 +20,7 @@ from typing import Any
 
 from click import prompt
 from click.exceptions import Abort
+
 from testscribe.eval_expression import eval_expression
 from testscribe.log import log
 from testscribe.reflection_util import get_type_name
