dc133775b94cf848d150c1b9fdda844e4b69ed70
Ray Yang <ruiguo@gmail.com>
2022-10-19 16:18:59 -0700

    Remove two wrapper based scribe and test files
    Somehow Github's test run output is formatted differently.

--- tests/generated/wrapper/test_exception_decorator_wrapper_g.py
+++
@@ -1,6 +0,0 @@
-from wrapper.exception_decorator_wrapper import run_dummy_app
-
-
-def test_run_dummy_app():
-    result = run_dummy_app()
-    assert result == (1, 'Aborted due to an exception:\nTraceback (most recent call last):\n  File "src/testscribe/exception_decorator.py", line 42, in wrapper_decorator\n    return func(*args, **kwargs)\n  File "tests/test_data/dummy_app.py", line 10, in raise_exception\n    raise Exception("Test exception")\nException: Test exception\n\nAborted.\n')
